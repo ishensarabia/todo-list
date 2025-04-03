88a77a2c08e54519bcf68a514d23cd6ef36c508b
sci-punk <ishen.sarabia@gmail.com>
2025-04-03 14:27:14 -0600

    add package

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: 'src/index.html',
+        }),
+    ],
+    
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+        ],
+    },
+};
